b7bd6fcac23ecf2e1e3cf9c3051aa933453e7dfe
jdev <j.developer.days@gmail.com>
2025-08-08 11:24:52 +0300

    angular create project folder structure

---
+++ angular_folder_structure.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+mkdir -p ./src/app/components/auxiliary
+mkdir -p ./src/app/components/main
+
+mkdir -p ./src/app/services
+
+mkdir -p ./src/assets/audio
+mkdir -p ./src/assets/i18n
+mkdir -p ./src/assets/images
+mkdir -p ./src/assets/videos
+
+mkdir -p ./src/environments
+
+tree ./src
