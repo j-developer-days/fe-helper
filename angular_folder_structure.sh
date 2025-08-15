dc6f6b0a817bcd750f25a868d003c4dc4b63b5a2
jdev <j.developer.days@gmail.com>
2025-08-15 12:51:17 +0300

    added to shell script create new folder for helpers

--- angular_folder_structure.sh
+++ angular_folder_structure.sh
@@ -5,6 +5,8 @@ mkdir -p ./src/app/components/main
 
 mkdir -p ./src/app/services
 
+mkdir -p ./src/app/helpers
+
 mkdir -p ./src/assets/audio
 mkdir -p ./src/assets/i18n
 mkdir -p ./src/assets/images
